bbd12736ca49fe612e7c6b86a70faff684ce930f
flowercordoba <flowercordoba7@gmail.com>
2025-08-22 21:44:12 -0500

    Add dotenv and express dependencies; update health module imports; create docker-compose file

--- src/features/health/health.module.ts
+++ src/features/health/health.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseHealthService } from '@shared/database/drizzle/services/database-health.service';
 import { HealthController } from './health.controller';
+import { DrizzleModule } from '@shared/database/drizzle/drizzle.module';
 
 @Module({
-    imports: [],
+    imports: [DrizzleModule],
     controllers: [HealthController],
     providers: [DatabaseHealthService],
     exports: [DatabaseHealthService],
