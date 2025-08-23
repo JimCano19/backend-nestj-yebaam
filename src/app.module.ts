ca085a76b79eeeb0b1c3eb14ff41cd592c96c21d
flowercordoba <flowercordoba7@gmail.com>
2025-08-22 23:14:25 -0500

    refactor: remove unused module imports from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { FeaturesModule } from './features/features.module';
-import { SharedModule } from '@shared/shared.module';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [
-    FeaturesModule,
-    SharedModule
-  ],
+ 
   controllers: [AppController],
   providers: [],
 })
